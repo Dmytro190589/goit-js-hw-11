86bd3de7ed380c5c3901e5b4014556d63d566b01
Dmytro Kharchenko <dmytro190589@gmail.com>
2022-11-11 14:35:00 +0200

    конец

---
+++ src/index.js
@@ -0,0 +1,95 @@
+import { fetchImages } from './js/fetch-api.js';
+import { createMarkupCard } from './js/markup.js';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+
+let userSearchInput = null;
+let previousSearchInput = null;
+let page = 1;
+let lightbox = null;
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+
+  userSearchInput = e.currentTarget['searchQuery'].value;
+  if (previousSearchInput !== userSearchInput) {
+    refs.gallery.innerHTML = '';
+    page = 1;
+  }
+
+  fetchImages(userSearchInput, page)
+    .then(res => {
+      if (res.data.totalHits === 0) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+        Notiflix.Notify.info(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+      const newObj = res.data.hits;
+      newObj.forEach(function (img) {
+        createMarkupCard(img);
+      });
+      lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 600,
+      });
+      Notiflix.Notify.info(`Hooray! We found ${res.data.totalHits} images.`);
+
+      if (newObj.length === 40) {
+        refs.loadMoreBtn.classList.remove('is-hidden');
+      }
+
+      const { height: cardHeight } =
+        refs.gallery.firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 0.35,
+        behavior: 'smooth',
+      });
+    })
+    .catch(console.log);
+
+  previousSearchInput = userSearchInput;
+  e.currentTarget.reset();
+}
+
+function handleLoadMoreBtnClick(e) {
+  e.preventDefault();
+
+  page += 1;
+  fetchImages(userSearchInput, page)
+    .then(res => {
+      const newObj = res.data.hits;
+
+      newObj.forEach(function (img) {
+        createMarkupCard(img);
+      });
+      lightbox.refresh();
+      if (newObj.length < 40) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+        Notiflix.Notify.info(
+          `We're sorry, but you've reached the end of search results.`
+        );
+      }
+
+      const { height: cardHeight } =
+        refs.gallery.firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2.5,
+        behavior: 'smooth',
+      });
+    })
+    .catch(console.log);
+}
+
+refs.form.addEventListener('submit', handleFormSubmit);
+refs.loadMoreBtn.addEventListener('click', handleLoadMoreBtnClick);
---
+++ src/js/markup.js
@@ -0,0 +1,37 @@
+const refs = {
+    gallery: document.querySelector('.gallery'),
+  };
+  
+  export function createMarkupCard({
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+  }) {
+    refs.gallery.insertAdjacentHTML(
+      'beforeend',
+      `
+    <div class="photo-card">
+    <a href="${largeImageURL}">
+    <img class="card-img" src="${webformatURL}" alt="${tags}" loading="lazy"/>
+    </a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>${downloads}
+      </p>
+    </div>
+  </div>`
+    );
+  }
